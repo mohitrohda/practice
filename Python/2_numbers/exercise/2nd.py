dcb0e18c70841a23db5ccbacd71c6e6808ee6b6d
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-12 23:13:46 +0530

    Create 2nd.py

---
+++ Python/2_numbers/exercise/2nd.py
@@ -0,0 +1,11 @@
+#You bought 9 packets of potato chips from a store. Each packet costs 1.49 dollar and you gave shopkeeper 20 dollar.
+#Find out using python, how many dollars is the shopkeeper going to give you back?
+
+potato_chips = 9
+price_of_one_potato_chips= 1.49
+
+total_price_of_potato_chips = potato_chips * price_of_one_potato_chips
+
+return_money_by_shopkeeper = 20 - total_price_of_potato_chips
+
+print("Shopkeeper Return money =",return_money_by_shopkeeper)
