d8229028e5206e3122b50bbb45c24e3d086ff2ad
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-12 23:13:37 +0530

    Create 2_numbers.py

---
+++ Python/2_numbers/2_numbers.py
@@ -0,0 +1,25 @@
+'''
+print(11+22)  #addition
+print(11-22)  #subtraction
+print(11/22)  #division
+print(11*22)  #multiplication
+print(11%22)  #remainder
+print(11**22)  #power
+
+'''
+
+rpr_drg= 40
+drg_rjn= 30
+
+kmh= 40
+
+total_distance = rpr_drg + drg_rjn
+time= total_distance/kmh
+round_time = round(time,2)
+print("Total Distance =",total_distance)
+print("time of Travelling =", round_time)
+
+# print(6-5.7) output 0.299999999
+
+round_no = round(6-5.7,2)
+print(round_no)
