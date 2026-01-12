7f2f78005c61a8c3f9a6cae59f99d278c6f00357
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-13 00:45:30 +0530

    Create 3_string_2.py

---
+++ Python/3_string/3_string_2.py
@@ -0,0 +1,25 @@
+s1="hello" 
+s2="world"
+print(s1+s2)
+print(s1+' '+s2)
+
+s= "Total Stud in Class = "
+total_stud = 40
+
+# print(s+num) error
+num = str(total_stud)
+print(s+num)
+
+
+
+'''
+error bcz of assignment operator
+
+s= "Total Stud in Class = "
+num= 40
+
+str(num)
+
+print(s+num)
+'''
+
