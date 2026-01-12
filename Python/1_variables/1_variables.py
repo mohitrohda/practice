65b64143fd1b4453852b9039391baf1b8e4bcc2b
Mohit Rohda <snehalsingh.work@gmail.com>
2026-01-12 23:08:27 +0530

    Create 1_variables.py

---
+++ Python/1_variables/1_variables.py
@@ -0,0 +1,16 @@
+rent = 7000
+gas = 3000
+groc=5000
+
+print(rent)
+
+total= rent+gas+groc
+print(total)
+
+rent= 8000
+print(total)
+
+item1="rent"
+item2="gas"
+item3="groc"
+print("Expense item:",item1, item2, item3)
