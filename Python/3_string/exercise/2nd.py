9091b89122f55994c9f9aca106653b17041f4874
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-13 00:45:35 +0530

    Create 2nd.py

---
+++ Python/3_string/exercise/2nd.py
@@ -0,0 +1,8 @@
+#Create a variable to store the string "Earth revolves around the sun"
+#Print "revolves" using slice operator
+#Print "sun" using negative index
+
+string = "Earth revolves around the sun"
+
+print(string[6:14])
+print(string[-3:])
