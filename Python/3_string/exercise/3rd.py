f96678914501a2dbae7f1f1b394a605bb39caab5
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-13 00:45:38 +0530

    Create 3rd.py

---
+++ Python/3_string/exercise/3rd.py
@@ -0,0 +1,8 @@
+#Create two variables to store how many fruits and vegetables you eat in a day.
+#Now Print "I eat x veggies and y fruits daily" where x and y presents vegetables and fruits that you eat everyday.
+#Use python f string for this.
+
+x = 3 #veggie
+y = 2 #fruit
+
+print(f"I eat {x} veggies and {y} fruits daily")
