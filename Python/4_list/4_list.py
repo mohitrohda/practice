2a34f0c56b439fb672cb5e1eb70133a9b387d7ac
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-15 20:19:01 +0530

    Create 4_list.py

---
+++ Python/4_list/4_list.py
@@ -0,0 +1,22 @@
+items = ["bread","pasta","fruits","vegetables"]
+print(items[0])
+print(items[3])
+
+#modifying list 
+items[0]='chips'
+print(items)
+
+print(items[0:3])
+
+print(items[-3])
+print(items[-3:-1])
+
+
+print ("pasta" in items)
+
+items.append('butter')
+items.insert(2,'mggie')
+
+print(items)
+
+print(len(items))
