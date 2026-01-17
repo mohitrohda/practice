9e59a5de6621c6237b32dc6cf26a4fd271c7099d
Mohit Rohda <163033754+mohitrohda@users.noreply.github.com>
2026-01-17 22:58:20 +0530

    Update 0_RoughWork.py

---
+++ Python/0_RoughWork.py
@@ -0,0 +1,16 @@
+a = 3 #int(input("enter 1st number: "))
+b = 4 #int(input("enter 2nd number: "))
+
+sum = a+b
+
+print(sum)
+
+name = "Mohit rohda"
+
+print(name.find('r'))
+
+print(name.replace("Mohit","Snehal"))
+
+print(name)
+
+print('M' in name)
